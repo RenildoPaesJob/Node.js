6e6bac252fef54800f966a644d8fb5a46e52cef3
Renildo Paes <renildopaesJoob@gmail.com>
2022-08-22 18:37:54 -0300

    ok

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-const express     = require('express')
-let   routesIndex = require('./routes/index')
-let   routesUsers = require('./routes/users')
+const express          = require("express");
+const consign          = require("consign");
+const bodyParser       = require("body-parser");
+const expressValidator = require("express-validator");
 
-let app = express()
+let app = express();
 
-app.use(routesIndex)
-app.use('/users', routesUsers)
+//interpretar os urls 
+app.use(bodyParser.urlencoded({ extended: false }));
+//transformar os dados em json
+app.use(bodyParser.json());
 
-app.listen(3000, '127.0.0.1', () => {
-  console.log('servidor rodando!')
-})
+app.use(expressValidator());
+
+//.include() => vai incluir todos os arquivos da pasta 'routes'
+//.into() => dentro do 'APP'
+//mudando a estruturas dos arquivos de routas para dentro de uma função
+consign().include("routes").include('utils').into(app);
+
+//terminal
+app.listen(3000, "127.0.0.1", () => {
+  console.log("servidor rodando!");
+});
--- routes/users.js
+++ routes/users.js
@@ -1,26 +1,88 @@
-const { application } = require('express')
-let express = require('express')
-let app     = express.Router()
+let NeDB = require("nedb");
+let db = new NeDB({
+  filename: "users.db",
+  autoload: true,
+});
 
-app.get('/', (req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  // res.end(JSON.stringify({
-  res.json({
-    users: {
-      name: 'Renildo',
-      email: 'renildo@gmail.com',
-      id: 1
+module.exports = (app) => {
+
+  let route   = app.route('/users');
+  let routeId = app.route('/users/:id');
+
+  route.get((req, res) => {
+    //find => procurar, sem parametros, busca todos os registros
+    //sort => ordenar = 1 => decrescente, -1 => crescente, name => ordenar por name
+    //exec => executa um função de callback.
+    db.find({}).sort({name:1}).exec((err, users) => {
+      if (err) {
+        //function da pasta utils, metodo send()
+        app.utils.error.send(err, req, res);
+      } else {
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "application/json");
+        // res.end(JSON.stringify({
+        res.json({
+          users
+        });
+      }
+    });
+  });
+
+  route.post((req, res) => {
+    /**VALIDAÇÕES*/
+    req.assert('name', 'O nome é obrigatório!').notEmpty();
+    req.assert('email', 'O email está invalido!').notEmpty().isEmail();
+
+    let errors = req.validationErrors();
+    
+    if (errors) {
+      //function da pasta utils, metodo send()
+      app.utils.error.send(errors, req, res);
+      return false;
     }
-  })
-})
 
-app.get('/admin', (req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'application/json')
-  res.json({
-    users: []
-  })
-})
+    //usando NEDB para armazenar os dados do users
+    db.insert(req.body, (err, user) => {
+      if (err) {
+        //function da pasta utils, metodo send()
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(user);
+      }
+    });
+  });
 
-module.exports = app
+  //buscar um registro
+  routeId.get((req, res) => {
+    //findOne => busca um registro
+    db.findOne({_id:req.params.id}).exec((err, user) => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(user);
+      }
+    });
+  });
+
+  //editar um registro
+  routeId.put((req, res) => {
+    db.update({_id: req.params.id}, req.body, err => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(Object.assign(req.params, req.body));
+      }
+    });
+  });
+
+  //excluir um registro
+  routeId.delete((req, res) => {
+    db.remove({_id: req.params.id}, {}, err => {
+      if (err) {
+        app.utils.error.send(err, req, res);
+      } else {
+        res.status(200).json(req.params);
+      }
+    });
+  });
+};

5e098616d8acc973fa6b1128f0f95ebfa94edf07
Renildo Paes <renildopaesJoob@gmail.com>
2022-08-26 18:27:08 -0300

    first commit

---
+++ public/index.js
@@ -0,0 +1,15 @@
+//PEGA TODOS OS CAMPOS DENTRO DE UM FORMULARIO E ATRIBUI DENTRO DE UMA UNICA VARIAVEL
+// let fields = document.querySelectorAll("#form-user-create [name]")
+
+//DECLARANDO UM NOVO OBJETO JSON
+// let user = {}
+
+/**
+ * SPA => Single Page Application
+ * --------------------------------
+ * Aplicação de página unica
+*/
+
+//new object UserController
+let userController = new UserController("form-user-create", "form-user-update", "table-users");
+//OOI

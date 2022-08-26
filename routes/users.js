4b312ff667d358777d0933e8bd3b29b5e120a12c
Renildo Paes <renildopaesJoob@gmail.com>
2022-08-26 18:43:33 -0300

    bug node

--- routes/users.js
+++ routes/users.js
@@ -9,11 +9,54 @@ var client = restify.createJsonClient({
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+
   client.get('/users', (err, request, response, obj) => {
+
     assert.ifError(err);
     
-    res.end(JSON.stringify(obj, null, 2));
+    res.end(obj);
   })
+});
+
+router.get('/:id', function(req, res, next) {
+
+  client.get(`/users/${req.params.id}`, (err, request, response, obj) => {
+
+    assert.ifError(err);
+    
+    res.end(obj);
+  });
+});
+
+router.put('/:id', function(req, res, next) {
+
+  client.put(`/users/${req.params.id}`, req.body, (err, request, response, obj) => {
+
+    assert.ifError(err);
+    
+    res.end(obj);
+  });
+});
+
+router.delete('/:id', function(req, res, next) {
+
+  //o method delete no restify, so usa as 3 primeiras letras do delete
+  client.del(`/users/${req.params.id}`, (err, request, response, obj) => {
+
+    assert.ifError(err);
+    
+    res.end(obj);
+  });
+});
+
+router.post('/', function(req, res, next) {
+
+  client.post(`/users`, req.body, (err, request, response, obj) => {
+
+    assert.ifError(err);
+    
+    res.end(obj);
+  });
 });
 
 module.exports = router;
